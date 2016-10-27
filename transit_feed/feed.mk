0d80a24e49b97e8427b2eca8c7d57068ad3508c1
Federico Ressi <federico.ressi@intel.com>
2016-10-27 19:53:59 +0100

    Improve deploy scripts.

--- transit_feed/feed.mk
+++ transit_feed/feed.mk
@@ -4,13 +4,18 @@ install: install_feed
 
 .PHONY: build_feed install_feed
 
-build_feed:
-	python -m transit_feed --target index --debug --build-dir $(BUILD_DIR)/feed
-
 include $(shell python -m transit_feed --target makefile --debug \
     --build-dir $(BUILD_DIR)/feed)
 
+build_feed:
+	@echo Build index file
+	python -m transit_feed --target index --debug --build-dir $(BUILD_DIR)/feed
+
 install_feed:
+	@echo Install feed files
+	rm -fR $(INSTALL_DIR)/tmp
+	mkdir -p $(INSTALL_DIR)/tmp
+	cp -fR $(BUILD_DIR)/feed $(INSTALL_DIR)/tmp
 	rm -fR $(INSTALL_DIR)/feed
-	mkdir -p $(INSTALL_DIR)/feed
-	cp -fR $(BUILD_DIR)/feed $(INSTALL_DIR)
+	mv $(INSTALL_DIR)/tmp/feed $(INSTALL_DIR)
+	rm -fR $(INSTALL_DIR)/tmp
